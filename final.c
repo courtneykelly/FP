f8dea977ff202b871497bfbae4ffd3a9f3e50cb4
Courtney Kelly <ckelly19@nd.edu>
2015-12-09 21:32:00 -0500

    Changed from sheep to pig

--- final.c
+++ final.c
@@ -9,15 +9,15 @@
 #include <stdlib.h>
 #include "gfx4.h"
 
-typedef struct sheep {
+typedef struct pig {
   int xcenter;
   int ycenter;
   double vx;
   double vy;
-} sheep_t;
+} pig_t;
 
-void drawsheep(int,int);
-void animatesheep(sheep_t sheep[], int, int, int);
+void drawpig(int,int);
+void animatepig(sheep_t sheep[], int, int, int);
 
 int main(void){
 
@@ -25,36 +25,29 @@ int main(void){
   int width = 750;
   int i;
   int loop=1;
-  int randomSpeeds[3]= {0,-1,1};
-  sheep_t sheep[5];
-  gfx_open(width,height,"Sheep Game");
+  double randomSpeeds[3]= {0,-1,1};
+  sheep_t pig[5];
+  gfx_open(width,height,"Save the Swine");
 
-
-
-  //initial position and speed
+  //initial position and speed of pig 
   for(i=0;i<5;i++) {
-    sheep[i].xcenter=rand() % width;
-    sheep[i].ycenter=rand() % height;
-    sheep[i].vx= randomSpeeds[rand() % 2];
-    sheep[i].vy= randomSpeeds[rand() % 2];
+    pig[i].xcenter=rand() % width;
+    pig[i].ycenter=rand() % height;
+    pig[i].vx= randomSpeeds[rand() % 2];
+    pig[i].vy= randomSpeeds[rand() % 2];
   }
 
- 
-
-
-  while(loop) {
-
-    gfx_clear();
-
-    for(i=0;i<4;i++) {
-      animatesheep(sheep,i,width,height);
+  // Animation Loop
+    while(loop) {
+      gfx_clear();
+      for(i=0;i<4;i++) {
+        animatepig(pig,i,width,height);
+      }
     }
-  }
 
 }
 
-
-void drawsheep(int xcenter, int ycenter){
+void drawpig(int xcenter, int ycenter){
   gfx_circle(xcenter,ycenter,20);
   gfx_circle(xcenter,ycenter,10);
   gfx_circle(xcenter+3,ycenter-2,2);
@@ -67,22 +60,22 @@ void drawsheep(int xcenter, int ycenter){
 
 }
 
-void animatesheep(sheep_t sheep[],int i, int width, int height){
+void animatepig(pig_t pig[], int i, int width, int height){
 
-  drawsheep(sheep[i].xcenter,sheep[i].ycenter);
+  drawpig(pig[i].xcenter,pig[i].ycenter);
   gfx_flush();
-  sheep[i].xcenter=sheep[i].xcenter + sheep[i].vx * 0.1;
-  sheep[i].ycenter=sheep[i].ycenter + sheep[i].vy * 0.1;
+  pig[i].xcenter=pig[i].xcenter + pig[i].vx * 0.1;
+  pig[i].ycenter=pig[i].ycenter + pig[i].vy * 0.1;
 
   //wall collision detect
-  if(sheep[i].xcenter >= width)
-    sheep[i].vx = - sheep[i].vx;
-  else if( sheep[i].xcenter <= 20)
-    sheep[i].vx = -sheep[i].vx;
-  else if(sheep[i].ycenter >= height)
-    sheep[i].vy = -sheep[i].vy;
-  else if( sheep[i].ycenter <= 20)
-     sheep[i].vy = -sheep[i].vy;
+  if(pig[i].xcenter >= width)
+    pig[i].vx = - pig[i].vx;
+  else if( pig[i].xcenter <= 20)
+    pig[i].vx = -pig[i].vx;
+  else if(pig[i].ycenter >= height)
+    pig[i].vy = -pig[i].vy;
+  else if( pig[i].ycenter <= 20)
+    pig[i].vy = -pig[i].vy;
 
    gfx_flush();
    usleep(2000);
