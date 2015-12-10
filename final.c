43de7a91300611051bbde68c4dfdd369d44846ec
Kathleen Patricia Schermerhorn <kscherme@student00.cse.nd.edu>
2015-12-09 21:14:01 -0500

    Starting to animate

--- final.c
+++ final.c
@@ -2,8 +2,11 @@
 //Sheep Game
 //Katie Schermerhorn and Courtney Kelly
 
+
 #include <stdio.h>
 #include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "gfx4.h"
 
 typedef struct sheep {
@@ -14,28 +17,42 @@ typedef struct sheep {
 } sheep_t;
 
 void drawsheep(int,int);
-void animatesheep(double,double,int,int,int,int);
+void animatesheep(sheep_t sheep[], int, int, int);
 
 int main(void){
 
   int height= 750;
   int width = 750;
   int i;
+  int loop=1;
+  int randomSpeeds[3]= {0,-1,1};
+  sheep_t sheep[5];
   gfx_open(width,height,"Sheep Game");
 
+
+
+  //initial position and speed
   for(i=0;i<5;i++) {
     sheep[i].xcenter=rand() % width;
     sheep[i].ycenter=rand() % height;
-    drawsheep(sheep[i].xcenter , sheep[i].ycenter);
+    sheep[i].vx= randomSpeeds[rand() % 2];
+    sheep[i].vy= randomSpeeds[rand() % 2];
   }
 
+ 
 
 
-  while(1) {
+  while(loop) {
 
+    gfx_clear();
 
+    for(i=0;i<4;i++) {
+      animatesheep(sheep,i,width,height);
+    }
+  }
 
 }
+
 
 void drawsheep(int xcenter, int ycenter){
   gfx_circle(xcenter,ycenter,20);
@@ -48,25 +65,26 @@ void drawsheep(int xcenter, int ycenter){
   gfx_line(xcenter+(10*sqrt(2)),ycenter+(10*sqrt(2)),xcenter+(10*sqrt(2))+4,ycenter+(10*sqrt(2))+4);
   gfx_line(xcenter-(10*sqrt(2)),ycenter+(10*sqrt(2)),xcenter-(10*sqrt(2))-4,ycenter+(10*sqrt(2))+4);
 
-
-
 }
 
-void animatesheep(double vx, double vy, int xcenter, int ycenter, int width, int height){
+void animatesheep(sheep_t sheep[],int i, int width, int height){
 
-  gfx_flush();                                                                                          
-  xcenter = xcenter + vx*.01;                                                                          
-  ycenter = ycenter + vy*.01;
+  drawsheep(sheep[i].xcenter,sheep[i].ycenter);
+  gfx_flush();
+  sheep[i].xcenter=sheep[i].xcenter + sheep[i].vx * 0.1;
+  sheep[i].ycenter=sheep[i].ycenter + sheep[i].vy * 0.1;
 
-  drawsheep(xcenter,ycenter);                                                                           
+  //wall collision detect
+  if(sheep[i].xcenter >= width)
+    sheep[i].vx = - sheep[i].vx;
+  else if( sheep[i].xcenter <= 20)
+    sheep[i].vx = -sheep[i].vx;
+  else if(sheep[i].ycenter >= height)
+    sheep[i].vy = -sheep[i].vy;
+  else if( sheep[i].ycenter <= 20)
+     sheep[i].vy = -sheep[i].vy;
 
-  //wall collision detection                                                                             
-  if(xcenter >= width)                                                                                  
-  vx = -vx;                                                                                            
-  else if(ycenter >= height)                                                                            
-  vy = -vy;                                                                                                                                                                                         
-
-  gfx_flush();                                                                                          
-  usleep(2000);  
-
-}
+   gfx_flush();
+   usleep(2000);
+                                                  
+ }
